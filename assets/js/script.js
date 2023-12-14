c4b197228d99b661a281879a55e15f2bca1fefe9
jeanaltarejos <jeanaltarejos20@gmail.com>
2023-12-14 16:03:47 +0800

    Finished Sunnyside Landing Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
-$(document).ready(function (){
-    $('.wrapper-text').addClass('.blue');
+//hamburger nav menu
+
+$('.hamburger').on("click", function(e){
+    $('.hamburger').toggleClass("active");
+    $('.navmenu').toggleClass("active");
+});
+
+
+//graphic design and photography font colors
+$(document).ready(function(){
+    $('#1').children('h2').addClass('blue');
+    $('#1').children('p').addClass('blue');
+    $('#2').children('h2').addClass('green');
+    $('#2').children('p').addClass('green');
+    console.log("End of function");
 })
+
